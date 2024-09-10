360de2522c8d1cfffcebe789e89c76e57c58cc9c
Vijitverm1234 <vijitverma78@gmail.com>
2024-09-10 22:51:54 +0530

    please commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const a = document.querySelector("#bar");
+const b = document.querySelector("#bar");
+const c = document.querySelector("#openSide");
+function cut(){
+    
+    a.style.display="none";
+    c.style.display="flex"
+}
+function op(){
+    b.style.display="flex";
+    // a.style.display="none";
+    c.style.display="none"
+
+}
